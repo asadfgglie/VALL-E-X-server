20822f0b816c1e4d3e5704491009a7e82930161d
asadfgglie <ac121878@outlook.com>
2024-01-10 08:37:01 +0800

    add make prompt

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import uvicorn
 from fastapi import FastAPI
 from scipy.io.wavfile import write
 
-from schemas import SynthesizeQuery, SynthesizeQueryResult, MakePromptQuery, MakePromptResult
+from schemas import SynthesizeQuery, SynthesizeQueryResult, MakePromptQuery, MakePromptResult, SynthesizeLongQuery
 from vall_e_x.utils.generation import generate_audio, SAMPLE_RATE, generate_audio_from_long_text, preload_models
 from vall_e_x.utils.prompt_making import make_prompt as mpt
 
@@ -26,36 +26,42 @@ def hello_world():
 
 @app.post('/synthesize', response_model=SynthesizeQueryResult)
 def synthesize(query: SynthesizeQuery):
-    t1 = time.time()
-    data = (generate_audio(query.text, query.prompt_name, query.lang, query.accent) * 32767).astype(np.int16)
-    write('output.wav', SAMPLE_RATE, data)
-    logging.info(f'Time cost: {time.time() - t1}')
-    with open('output.wav', 'rb') as f:
-        return dict(base64_audio=base64.b64encode(f.read()).decode())
+    try:
+        t1 = time.time()
+        data = (generate_audio(query.text, query.prompt_name, query.lang, query.accent) * 32767).astype(np.int16)
+        write('output.wav', SAMPLE_RATE, data)
+        logging.info(f'Time cost: {time.time() - t1}')
+        with open('output.wav', 'rb') as f:
+            return SynthesizeQueryResult(base64_audio=base64.b64encode(f.read()).decode())
+    except Exception as e:
+        return SynthesizeQueryResult(result='failed', detail=str(e))
 
 @app.post('/synthesize_long', response_model=SynthesizeQueryResult)
-def synthesize_long(query: SynthesizeQuery):
-    t1 = time.time()
-    data = (generate_audio_from_long_text(query.text, query.prompt_name, query.lang, query.accent) * 32767).astype(np.int16)
-    write('output.wav', SAMPLE_RATE, data)
-    logging.info(f'Time cost: {time.time() - t1}')
-    with open('output.wav', 'rb') as f:
-        return dict(base64_audio=base64.b64encode(f.read()).decode())
+def synthesize_long(query: SynthesizeLongQuery):
+    try:
+        t1 = time.time()
+        data = (generate_audio_from_long_text(query.text, query.prompt_name, query.lang, query.accent, query.mode) * 32767).astype(np.int16)
+        write('output.wav', SAMPLE_RATE, data)
+        logging.info(f'Time cost: {time.time() - t1}')
+        with open('output.wav', 'rb') as f:
+            return SynthesizeQueryResult(base64_audio=base64.b64encode(f.read()).decode())
+    except Exception as e:
+        return SynthesizeQueryResult(result='failed', detail=str(e))
 
 @app.post('/make_prompt', response_model=MakePromptResult)
 def make_prompt(query: MakePromptQuery):
-    os.makedirs(TMP_DIR, exist_ok=True)
-    with open(TMP_AUDIO, 'wb') as f:
-        f.write(base64.b64decode(query.base64_audio))
-
     try:
+        os.makedirs(TMP_DIR, exist_ok=True)
+        with open(TMP_AUDIO, 'wb') as f:
+            f.write(base64.b64decode(query.base64_audio))
+
         mpt(query.prompt_name, TMP_AUDIO, query.transcript)
-        return dict(result='success', prompt_name=query.prompt_name)
+        return MakePromptResult(prompt_name=query.prompt_name)
     except Exception as e:
-        return dict(result='failed', prompt_name=query.prompt_name)
+        return MakePromptResult(result='failed', detail=str(e))
 
 if __name__ == "__main__":
     logging.info('Loading model...')
     preload_models()
     logging.info('model loaded done.')
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=6666)
--- schemas.py
+++ schemas.py
@@ -1,22 +1,34 @@
 from typing import Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
+class QueryResult(BaseModel):
+    result: Literal['success', 'failed'] = Field(default='success')
+    detail: Optional[str] = Field(default=None,
+    description='When result is failed, it will show the detail, otherwise this will be null.')
 
 class SynthesizeQuery(BaseModel):
-    lang: Literal['auto', 'zh', 'ja', 'en', 'mix'] = 'auto'
-    text: str
-    accent: Literal['中文', '日本語', 'English', 'Mix', 'no-accent'] = 'no-accent'
-    prompt_name: Optional[str] = None
+    lang: Literal['auto', 'zh', 'ja', 'en', 'mix'] = Field(default='auto', examples=['en'])
+    text: str = Field(examples=['Hello, my name is Toku Lili!'])
+    accent: Literal['中文', '日本語', 'English', 'Mix', 'no-accent'] = Field(default='no-accent', examples=['English'])
+    prompt_name: Optional[str] = Field(default=None, examples=[None])
 
-class SynthesizeQueryResult(BaseModel):
-    base64_audio: str
+class SynthesizeLongQuery(SynthesizeQuery):
+    mode: Literal['sliding-window', 'fixed-prompt'] = Field(default='sliding-window', examples=['sliding-window'],
+    description="""
+    For long audio generation, two modes are available.
+    * fixed-prompt: This mode will keep using the same prompt the user has provided, and generate audio sentence by sentence.
+    * sliding-window: This mode will use the last sentence as the prompt for the next sentence, but has some concern on speaker maintenance.
+    """)
+
+class SynthesizeQueryResult(QueryResult):
+    base64_audio: Optional[str] = Field(default=None)
 
 class MakePromptQuery(BaseModel):
-    prompt_name: str
+    prompt_name: str = Field(examples=['test'])
     base64_audio: str
-    transcript: Optional[str] = None
+    transcript: Optional[str] = Field(default=None,
+    description="If doesn't provide, it will use whisper to transcript prompt audio.")
 
-class MakePromptResult(BaseModel):
-    result: Literal['success', 'failed']
-    prompt_name: str
+class MakePromptResult(QueryResult):
+    prompt_name: Optional[str] = Field(default=None)
